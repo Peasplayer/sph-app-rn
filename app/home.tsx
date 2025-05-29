87bce0dc4dd1284e62e8ce9665bb2b9e3f1fb3c6
Peasplayer <peasplayer@peasplayer.xyz>
2025-05-30 01:28:45 +0200

    Added alert badges to the home page

--- app/home.tsx
+++ app/home.tsx
@@ -1,15 +1,27 @@
-import {Pressable, StyleSheet} from "react-native";
-import React from "react";
+import {Pressable, StyleSheet, View} from "react-native";
+import {useState} from "react";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {router} from 'expo-router';
-import {Button, Text, useTheme} from "react-native-paper";
+import {Badge, Button, Text, useTheme} from "react-native-paper";
 import Cache from "@/lib/Cache";
 import * as SecureStore from "expo-secure-store";
 
 export default function Home() {
     const theme = useTheme();
 
-    const [clicker, setClicker] = React.useState<number>(0);
+    const [clicker, setClicker] = useState<number>(0);
+    const [alerts, setAlerts] = useState<{
+        "myLessons": {alerts: any, texts: string[]};
+        "schedule": {alerts: any, texts: string[]};
+        "messages": {alerts: any, texts: string[]};
+        "substitution": {alerts: any, texts: string[]};
+    }|undefined>();
+
+    if (alerts === undefined) {
+        Cache.currentSession.fetchAlerts().then((r: any) => {
+           setAlerts(r);
+        });
+    }
 
     return (
         <SafeAreaView
@@ -18,32 +30,40 @@ export default function Home() {
             <Pressable onPress={() => {setClicker(clicker + 1)}}>
                 <Text style={{margin: 15, fontWeight: "bold"}} variant={"titleLarge"}>SPH+</Text>
             </Pressable>
+            <View style={styles.button}>
+                <Button
+                    icon="timer-sand"
+                    mode={"contained"}
+                    onPress={() => router.navigate("/schedule")}
+                >Stundenplan</Button>
+                {alerts !== undefined && alerts.schedule !== undefined ? <Badge visible={true} style={{position: "absolute", top: -7, left: -7}}>{alerts.schedule.alerts}</Badge> : null}
+            </View>
+            <View style={styles.button}>
+                <Button
+                    icon="account-arrow-left-outline"
+                    mode={"contained"}
+                    onPress={() => router.navigate("/substitution")}
+                >Vertretungsplan</Button>
+                {alerts !== undefined && alerts.substitution !== undefined ? <Badge visible={true} style={{position: "absolute", top: -7, left: -7}}>{alerts.substitution.alerts}</Badge> : null}
+            </View>
+            <View style={styles.button}>
+                <Button
+                    icon="book-education-outline"
+                    mode={"contained"}
+                    onPress={() => router.navigate("/myLessons")}
+                >Mein Unterricht</Button>
+                {alerts !== undefined && alerts.myLessons !== undefined ? <Badge visible={true} style={{position: "absolute", top: -7, left: -7}}>{alerts.myLessons.alerts}</Badge> : null}
+            </View>
+            <View style={styles.button}>
+                <Button
+                    icon="message-text-outline"
+                    mode={"contained"}
+                    onPress={() => router.navigate("/messages")}
+                >Nachrichten</Button>
+                {alerts !== undefined && alerts.messages !== undefined ? <Badge visible={true} style={{position: "absolute", top: -7, left: -7}}>{alerts.messages.alerts}</Badge> : null}
+            </View>
             <Button
-                style={styles.button}
-                icon="timer-sand"
-                mode={"contained"}
-                onPress={() => router.navigate("/schedule")}
-            >Stundenplan</Button>
-            <Button
-                style={styles.button}
-                icon="account-arrow-left-outline"
-                mode={"contained"}
-                onPress={() => router.navigate("/substitution")}
-            >Vertretungsplan</Button>
-            <Button
-                style={styles.button}
-                icon="book-education-outline"
-                mode={"contained"}
-                onPress={() => router.navigate("/myLessons")}
-            >Mein Unterricht</Button>
-            <Button
-                style={styles.button}
-                icon="message-text-outline"
-                mode={"contained"}
-                onPress={() => router.navigate("/messages")}
-            >Nachrichten</Button>
-            <Button
-                style={styles.button}
+                style={[styles.button, {marginTop: 15}]}
                 icon="logout"
                 mode={"contained-tonal"}
                 onPress={() => {
